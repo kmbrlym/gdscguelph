4e72ff3bdd2badefdb83d13b0b7bd46586525082
danlu <daniel.lu2@uwaterloo.ca>
2024-05-04 17:51:59 -0400

    ui changes

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ app.secret_key = 'your_secret_key_here'
 def index():
     return render_template("index.html", app_data=app_data)
 
+@app.route("/upload.html")
+def upload():
+    return render_template("upload.html", app_data=app_data)
+
 if __name__ == "__main__":
     app.run(debug=True)
