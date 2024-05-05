fae30db8217b44faf3cbd1ad0042d1f773205983
Kimberly Michela <kimberly@Kimberlys-MacBook-Pro.local>
2024-05-04 20:01:27 -0400

    updating

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import *
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -9,9 +10,24 @@ app.secret_key = 'your_secret_key_here'
 def index():
     return render_template("index.html", app_data=app_data)
 
-@app.route("/upload.html")
+@app.route("/upload", methods=['GET', 'POST'])
 def upload():
-    return render_template("upload.html", app_data=app_data)
+    if request.method == 'POST':
+        try:
+            # Get the uploaded files
+            marking_scheme_file = request.files['file1']
+            student_answer_file = request.files['file2']
 
+            # Save the uploaded files as PDFs
+            marking_scheme_file.save(os.path.join('static', 'markingscheme.pdf'))
+            student_answer_file.save(os.path.join('static', 'studentanswer.pdf'))
+
+            # Redirect or render a success page
+            return 'Files uploaded successfully!'
+        except Exception as e:
+            return f'An error occurred: {str(e)}'
+    else:
+        return render_template('upload.html', app_data=app_data)
+    
 if __name__ == "__main__":
     app.run(debug=True)
